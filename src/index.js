99b88cd58f9c08ac30c76a49c7a30fadb78df86a
jacobcalv <jacobcalvino@outlook.com>
2019-10-24 12:42:51 -0500

    directory indexes set up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+//dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+//App
 import App from './App';
-
+//indexes
+import rootReducer from './reducers'
+//styling
 import 'bulma/css/bulma.css';
 import './styles.scss';
+//Store
+const store = createStore(rootReducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, rootElement);
